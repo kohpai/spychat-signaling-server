15f027adef9ded6718effa737a800821980e44bc
Kohpai <whcwhc78@gmail.com>
2022-10-29 21:09:38 +0200

    :tada:

---
+++ src/main/kotlin/me/kohpai/Application.kt
@@ -0,0 +1,13 @@
+package me.kohpai
+
+import io.ktor.server.application.*
+import me.kohpai.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSockets()
+    configureRouting()
+}

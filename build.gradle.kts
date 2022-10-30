ee16b7795194ad412a5861bf688f0bd8072452b1
Kohpai <whcwhc78@gmail.com>
2022-10-30 16:10:40 +0100

    :heavy_plus_sign: bcpkix

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("org.bouncycastle:bcprov-jdk18on:$bc_version")
+    implementation("org.bouncycastle:bcpkix-jdk18on:$bc_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }

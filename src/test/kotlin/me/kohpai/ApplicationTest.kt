ba41da7f42781ff799cc9a9d826e8d6f64bef558
Kohpai <whcwhc78@gmail.com>
2022-10-30 16:13:03 +0100

    :recycle: CryptoTest as a separate class

--- src/test/kotlin/me/kohpai/ApplicationTest.kt
+++ src/test/kotlin/me/kohpai/ApplicationTest.kt
@@ -6,13 +6,6 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import io.ktor.websocket.*
-import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo
-import org.bouncycastle.openssl.PEMKeyPair
-import org.bouncycastle.openssl.PEMParser
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
-import java.io.File
-import java.security.interfaces.ECPrivateKey
-import java.security.interfaces.ECPublicKey
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -23,32 +16,6 @@ class ApplicationTest {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello World!", bodyAsText())
         }
-    }
-
-    @Test
-    fun testReadPrivatePemKeys() {
-        val keyConverter = JcaPEMKeyConverter()
-
-        val privateKeyFile =
-            File("src/test/resources/ec_private.pem").bufferedReader()
-        val privateKeyPem = PEMParser(privateKeyFile).readObject()
-        val keyPair = keyConverter.getKeyPair(privateKeyPem as PEMKeyPair)
-        val privateKey = keyPair.private as ECPrivateKey
-
-        assertEquals(384, privateKey.params.curve.field.fieldSize)
-    }
-
-    @Test
-    fun testReadPublicPemKeys() {
-        val keyConverter = JcaPEMKeyConverter()
-
-        val publicKeyFile =
-            File("src/test/resources/ec_public.pem").bufferedReader()
-        val publicKeyPem = PEMParser(publicKeyFile).readObject()
-        val publicKey =
-            keyConverter.getPublicKey(publicKeyPem as SubjectPublicKeyInfo) as ECPublicKey
-
-        assertEquals(384, publicKey.params.curve.field.fieldSize)
     }
 
     @Test

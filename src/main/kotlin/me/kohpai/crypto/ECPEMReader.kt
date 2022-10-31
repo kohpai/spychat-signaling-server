ad60c236ffe39281662088d8ab5ee1625ee44590
Kohpai <whcwhc78@gmail.com>
2022-10-31 14:00:15 +0100

    :bug: hardcoded readers

--- src/main/kotlin/me/kohpai/crypto/ECPEMReader.kt
+++ src/main/kotlin/me/kohpai/crypto/ECPEMReader.kt
@@ -4,7 +4,6 @@ import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo
 import org.bouncycastle.openssl.PEMKeyPair
 import org.bouncycastle.openssl.PEMParser
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
-import java.io.File
 import java.io.Reader
 import java.security.interfaces.ECPrivateKey
 import java.security.interfaces.ECPublicKey
@@ -14,18 +13,14 @@ class ECPEMReader {
         private val keyConverter = JcaPEMKeyConverter()
 
         fun readECPrivateKey(reader: Reader): ECPrivateKey {
-            val privateKeyFile =
-                File("src/test/resources/ec_private.pem").bufferedReader()
-            val privateKeyPem = PEMParser(privateKeyFile).readObject()
+            val privateKeyPem = PEMParser(reader).readObject()
             val keyPair = keyConverter.getKeyPair(privateKeyPem as PEMKeyPair)
 
             return keyPair.private as ECPrivateKey
         }
 
         fun readECPublicKey(reader: Reader): ECPublicKey {
-            val publicKeyFile =
-                File("src/test/resources/ec_public.pem").bufferedReader()
-            val publicKeyPem = PEMParser(publicKeyFile).readObject()
+            val publicKeyPem = PEMParser(reader).readObject()
 
             return keyConverter.getPublicKey(publicKeyPem as SubjectPublicKeyInfo) as ECPublicKey
         }

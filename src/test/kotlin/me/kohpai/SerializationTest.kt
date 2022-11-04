47bed6ce3b8b5aaf6a8eafce438a7130c4aaec7f
Kohpai <whcwhc78@gmail.com>
2022-11-04 11:01:51 +0100

    :white_check_mark: SerializationTest

---
+++ src/test/kotlin/me/kohpai/SerializationTest.kt
@@ -0,0 +1,29 @@
+package me.kohpai
+
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SerializationTest {
+    @Test
+    fun testSerializePacket() {
+        val now = ZonedDateTime.now()
+        val nowStr = now.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+        val pubKey = "my_pub_key"
+        val data = "my_data"
+        val connectPacket = Packet.connect(pubKey, now)
+        assertEquals(
+            """{"cmd":"CNT","pubKey":"$pubKey","signedAt":"$nowStr"}""",
+            Json.encodeToString(connectPacket)
+        )
+
+        val signalPacket = Packet.signal(pubKey, data, now)
+        assertEquals(
+            """{"cmd":"SGN","pubKey":"$pubKey","signedAt":"$nowStr","data":"$data"}""",
+            Json.encodeToString(signalPacket)
+        )
+    }
+}
